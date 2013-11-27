4f1bf7bdee45eb3e2ea8d4948a2958c8791a3f08
OpenShift guest <ar222da@student.lnu.se>
2013-11-27 08:23:14 -0500

    uppgift1

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,15 +2,46 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
-	
+	var secret = Math.floor(Math.random()*100)+1; // Detta tal behöver bytas ut mot ett slumpat tal.
+    var index = 0;
+
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
-		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
+   		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
+		
+    	    index++;
+    		
+    		if (number < 0 || number > 100)
+    		{
+    		    return [false, "Talet är utanför intervallet 0-100."];
+    		}
+    
+    		if (number < secret)
+    		{
+    		    return [false, "Det hemliga talet är högre!"];
+    		}
+    		
+    		if (number > secret)
+    		{
+    		    return [false, "Det hemliga talet är lägre!"];
+    		}
+    		
+    		if (isNaN(number))
+    		{
+    		    return [false, "Du måste ange ett tal mellan 0 - 100."];
+    		}
+    		
+    		else if (number == secret)
+    		{
+    		    return [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde " + index + " gissningar för att hitta det."];
+    		}
 
+		
+	
+		
+		// Plats för förändring.
+       
 
 		// Returnera exempelvis: 
 		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
