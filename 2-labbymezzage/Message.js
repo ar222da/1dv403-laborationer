b0335a45fbd334adfc227f8331bd4c8e0524185a
OpenShift guest <ar222da@student.lnu.se>
2013-12-19 10:03:58 -0500

    torsdag

--- 2-labbymezzage/Message.js
+++ 2-labbymezzage/Message.js
@@ -21,10 +21,87 @@ function Message(message, date){
     }
     
     Message.prototype.getHTMLText = function(){
-        
+        this.message = this.getText();
+        return this.message.replace(/[\n\r]/g, "<br/>");
     }
     
     Message.prototype.getDateText = function(){
+     
+    this.date = this.getDate();
+    var month;
+    
+        switch(this.date.getMonth())
+        {
+            case 0: month = "januari";
+                    break;
+            
+            case 1: month = "februari";
+                    break;
+            
+            case 2: month = "mars";
+                    break;
+            
+            case 3: month = "april";
+                    break;
+            
+            case 4: month = "maj";
+                    break;
+            
+            case 5: month = "juni";
+                    break;
+            
+            case 6: month = "juli";
+                    break;
+            
+            case 7: month = "augusti";
+                    break;
+            
+            case 8: month = "september";
+                    break;
+            
+            case 9: month = "oktober";
+                    break;
+            
+            case 10: month = "november";
+                    break;
+                
+            case 11: month = "december";
+                    break;
+            
+        }
+        
+        if (this.date.getHours() < 10)
+        {
+            var hours = "0" + this.date.getHours();
+        }
+        
+        else
+        {
+            hours = this.date.getHours();
+        }
+        
+        if (this.date.getMinutes() < 10) 
+        {
+            var minutes = "0" + this.date.getMinutes();
+        }
+        
+        else
+        {
+            minutes = this.date.getMinutes();
+        }
+        
+        if (this.date.getSeconds() < 10)
+        {
+            var seconds = "0" + this.date.getSeconds();
+        }
+        
+        else
+        {
+            seconds = this.date.getSeconds();
+        }
+        
+            
+        return "Inlägget skapades den " + this.date.getDate() + " " + month + " " + this.date.getFullYear() + " klockan " + hours + ":" + minutes + ":" + seconds;
         
     }
 
--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -1,23 +1,118 @@
 var MessageBoard = {
     
     messages: [],
-    
+
     init:function(e)
     {
         
     }
+
 }
 
+function showDate (dateButton) {
+    var messageID = dateButton.getAttribute("id");
+    var indexID = messageID.slice(4);
+        
+        alert(MessageBoard.messages[indexID].getDateText());
+    
+    renderMessages();
+}
+
+function removeMessage(removeButton) {
+    var messageID = removeButton.getAttribute("id");
+    var indexID = messageID.slice(6);
+    var conf = confirm("Vill du verkligen radera meddelandet?");
+        
+        if (conf == true)
+        {
+            MessageBoard.messages.splice(indexID, 1);
+        }
+    
+    renderMessages();
+}
+
+function renderMessage(messageID)
+{
+    var deletebutton = document.createElement("div");
+        
+        deletebutton.id = "delete" + messageID;
+        deletebutton.className = "delete";
+        
+    
+    var datebutton = document.createElement("div");
+    
+        datebutton.id = "date" + messageID;
+        datebutton.className = "date";
+    
+    var timefield = document.createElement("div");
+        timefield.id = "time";
+    
+    var time = document.createTextNode(MessageBoard.messages[messageID].getDateText().slice(46));
+        timefield.appendChild(time);
+        
+    var list = document.createElement("li");
+    var text = document.createElement("p");
+        
+        text.innerHTML = MessageBoard.messages[messageID].getHTMLText();
+        list.appendChild(text);
+        list.appendChild(datebutton);
+        list.appendChild(deletebutton);
+        list.appendChild(timefield);
+        document.getElementById("messages").appendChild(list);
+}
+
+function renderMessages()
+{
+    document.getElementById("messages").innerHTML = "";
+    
+        for (var i=0; i<MessageBoard.messages.length; i++)
+        {
+            renderMessage(i);    
+            document.getElementById("delete" + i).onclick = function(){
+                removeMessage(this);
+            }
+        
+            document.getElementById("date" + i).onclick = function(){
+                showDate(this);
+            }
+        
+        }
+        
+    document.getElementById("messagecounter").innerHTML = "Antal meddelanden: " + MessageBoard.messages.length;
+}
+
+function newMessage(){
+    var message = new Message();
+    
+        message.setText(document.getElementById("text").value);
+        message.setDate(new Date());
+        //console.log(message.getText());
+    
+    MessageBoard.messages.push(message);
+    renderMessages();
+}    
+    
+    
+
 window.onload = function (){
+    document.getElementById("messagecounter").innerHTML = "Antal meddelanden: " + MessageBoard.messages.length;
+    document.getElementById("messagebutton").onclick = newMessage;
     
-    var mess = new Message("Meddelande 1", new Date());
-    var mess2 = new Message("Meddelande 2", new Date());
-    var mess3 = new Message("Meddelande 3", new Date());
-    MessageBoard.messages[0] = mess;
-    MessageBoard.messages[1] = mess2;
-    alert(MessageBoard.messages[0].getText());
-    MessageBoard.messages.push(mess3);
-    alert(MessageBoard.messages[2].getText());
+    document.getElementById("text").onkeypress = function (e){
+        
+        if(!e) {
+            e =window.event;
+        }
+        
+        var bokstav = e.keyCode;
+
+        if (bokstav == 13 && e.shiftKey == false)
+        {
+            newMessage();
+        }
+          
+    }
     
+      
 }
 
